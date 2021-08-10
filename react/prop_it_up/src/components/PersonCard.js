e0475c3eba9a4fa140cebba966c9cabca8c08210
Nathan Denni <aubakasan303@gmail.com>
2021-08-10 17:39:52 +0900

    Putting it together assignment branch of prop_it_up

--- react/prop_it_up/src/components/PersonCard.js
+++ react/prop_it_up/src/components/PersonCard.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState }from 'react';
 
 const PersonCard = (props) => {
     const { firstName, lastName, age, hairColor } = props;
+    const [currentAge, setCurrentAge] = useState(age);
     return (
         <div>
             <h1>
                 {lastName}, {firstName}
             </h1>
-            <p>Age: {age}</p>
+            <p>Age: {currentAge}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={ (e) => setCurrentAge(currentAge + 1)}>It's {firstName} {lastName}'s' birthday!</button>
         </div>
     );
 }
