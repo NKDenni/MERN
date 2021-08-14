2714f81c48000616f73fe8d76412c0c0c8b8bff5
Nathan Denni <aubakasan303@gmail.com>
2021-08-14 17:30:19 +0900

    First commit of optional validations_form assignment - in progress

---
+++ react/validations_form/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './components/form'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
